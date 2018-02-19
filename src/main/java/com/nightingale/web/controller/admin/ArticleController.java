ff648c401cea51d78126598fcb842b6232bd7458
Hai Nguyen <nn.hai@samsung.com>
2018-02-19 10:12:33 +0800

    revert

--- src/main/java/com/nightingale/web/controller/admin/ArticleController.java
+++ src/main/java/com/nightingale/web/controller/admin/ArticleController.java
@@ -1,13 +1,18 @@
 package com.nightingale.web.controller.admin;
 
+import java.util.LinkedList;
+import java.util.List;
+
 import javax.validation.Valid;
 
+import org.apache.commons.lang3.tuple.Pair;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -16,15 +21,20 @@ import com.nightingale.app.entity.Article;
 import com.nightingale.app.model.dto.ArticleDTO;
 import com.nightingale.app.service.ArticleService;
 import com.nightingale.app.service.BaseService;
+import com.nightingale.web.util.Pagination;
 import com.nightingale.web.util.UtilValidation;
 
 @Controller
 @RequestMapping("/admin/article")
-public class ArticleController extends CRUDController<Article, ArticleDTO>{
+public class ArticleController{
 
 	private static final String ARTICLE_DTO = "articleDTO";
-	private static final String ARTICLE = "article";
+	private static final String MODEL = "article";
 	private static final String MODELS = "articles";
+	private static final String PAGINATION = "pagination";
+	private static final String KEYWORD = "keyword";
+	private static final String ERROR = "error";
+
 
 	private final static String FOLDER = "/admin/article";
 
@@ -34,46 +44,88 @@ public class ArticleController extends CRUDController<Article, ArticleDTO>{
 	@Value("${pageSize}")
 	private Integer pageSize = 10;
 	
-	@Override
-	protected BaseService<Article> getService() {
-		return articleService;
+	@GetMapping("")
+	public String home(Model model, @RequestParam(required = false, defaultValue = "") String keyword,
+			@RequestParam(required = false, defaultValue = "1") Integer pageNo) {
+
+		if (pageNo < 1)
+			pageNo = 1;
+
+		Pair<List<Article>, Integer> result = Pair.of(new LinkedList<>(), 0);
+
+		if (UtilValidation.isValidSearch(keyword)) {
+
+			result = articleService.getListWithPaginationBySearch(keyword, pageNo, pageSize);
+			model.addAttribute(MODELS, result.getLeft());
+
+		} else {
+			model.addAttribute(ERROR, "invalid_search");
+			keyword = "";
+		}
+
+		model.addAttribute(KEYWORD, keyword);
+		model.addAttribute(PAGINATION, new Pagination(pageNo, result.getRight(), pageSize));
+
+		return FOLDER + "/home";
+	}
+	
+	@GetMapping("/create")
+	public String create(Model model) {
+		model.addAttribute(MODEL, new Article());
+		return FOLDER + "/create";
 	}
 
-	@Override
-	protected int pageSize() {
-		return pageSize;
+	@PostMapping("/create")
+	public String create(Model model, @Valid Article article, BindingResult validResult) {
+
+		if (validResult.hasErrors()) {
+			return FOLDER + "/create";
+		} else {
+
+			articleService.create(article);
+			return "redirect:/admin/article";
+		}
+	}
+	
+	@GetMapping("/delete")
+	public String delete(Model model,
+			@RequestParam(name = "articleId", required = true, defaultValue = "-1") Integer articleId)
+			 {
+
+		if (UtilValidation.isValidId(articleId)) {
+
+			Article article = articleService.read(articleId);
+
+			if (article != null) {
+				model.addAttribute(ARTICLE_DTO, article);
+				return FOLDER + "/delete";
+			}
+
+		}
+		return "redirect:/admin/article";
+
 	}
 
-	@Override
-	protected String listingName() {
-		return MODELS;
-	}
+	@PostMapping("/delete")
+	public String delete(Model model, @ModelAttribute Article article)  {
 
-	@Override
-	protected String folder() {
-		return FOLDER;
-	}
+		if (article != null && article != null) {
 
-	@Override
-	protected String entityName() {
-		return ARTICLE;
-	}
+			Article m = articleService.read(article.getId());
 
-	@Override
-	protected String dtoName() {
-		return ARTICLE_DTO;
-	}
+			if (m != null) {
+				articleService.delete(m.getId());
+			} else {
+				model.addAttribute(ARTICLE_DTO, article);
+				return FOLDER + "/delete";
+			}
 
-	@Override
-	protected Article getNewEntity() {
-		return new Article();
-	}
+		}
 
-	@Override
-	protected Integer getEntityId(Article entity) {
-		return entity.getId();
-	}
+		return "redirect:/admin/article";
 
+	}
+	
 	@GetMapping("/details")
 	public String details(Model model,
 			@RequestParam(name = "articleId", required = true, defaultValue = "-1") Integer articleId)
